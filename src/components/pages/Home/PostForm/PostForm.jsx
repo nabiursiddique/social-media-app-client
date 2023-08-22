d79be1181a3ad316fc75147795a10d3617fe22b1
nabiursiddique <nabiursiddique01@gmail.com>
2023-08-22 16:36:30 +0600

    fix- Now user have to add a photo url instead of uploading it

--- src/components/pages/Home/PostForm/PostForm.jsx
+++ src/components/pages/Home/PostForm/PostForm.jsx
@@ -7,12 +7,13 @@ const PostForm = () => {
     const { user } = useContext(AuthContext);
 
     const handleAddPost = (data) => {
-        console.log(data.postContent);
+        console.log(data);
         reset();
     }
 
     return (
         <div className='mb-5'>
+
             <form onSubmit={handleSubmit(handleAddPost)}>
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 lg:gap-20 gap-4 lg:mx-32 md:mx-20 mx-8 mb-5'>
                     <div className='form-control w-full'>
@@ -24,12 +25,20 @@ const PostForm = () => {
                         })} placeholder="write here..." className="textarea textarea-bordered textarea-lg w-full max-w-lg" ></textarea>
                         {errors.postContent && <p className='text-sm mt-2 text-red-500'>{errors.postContent?.message}</p>}
                     </div>
-                    {/* For uploading Image */}
-                    <div className="form-control w-full max-w-xs my-auto">
+
+                    {/* For uploading Image, we will implement later*/}
+                    {/* <div className="form-control w-full max-w-xs my-auto">
                         <label className="label">
                             <span className="label-text text-blue-400">Upload a Photo</span>
                         </label>
                         <input {...register("postImage")} type="file" className="file-input file-input-bordered file-input-info w-full max-w-lg" />
+                    </div> */}
+
+                    {/* Photo url */}
+                    <div className="form-control w-full my-auto">
+                        <label className="label"><span className="label-text">Photo URL</span></label>
+                        <input {...register("photoURL")} type="text" placeholder="Your photoURL" className="input input-bordered w-full" />
+                        {errors.photoURL && <p className='text-sm mt-2 text-red-500'>{errors.photoURL?.message}</p>}
                     </div>
                 </div>
                 <div className='flex justify-center'>
@@ -46,6 +55,7 @@ const PostForm = () => {
                     }
                 </div>
             </form>
+
         </div>
     );
 };
